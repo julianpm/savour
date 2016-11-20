b6257b00a8426fc3e0347602994f47b1f13e3e69
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-20 14:43:37 -0500

    enqueued styles and scripts

--- functions.php
+++ functions.php
@@ -103,10 +103,12 @@ add_action( 'widgets_init', 'svr_widgets_init' );
  */
 function svr_scripts() {
 	wp_enqueue_style( 'svr-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'svr-app', get_template_directory_uri() . '/assets/css/app.css' );
 
 	wp_enqueue_script( 'svr-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'svr-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'svr-appjs', get_template_directory_uri() . '/assets/js/app.min.js', array('jquery'), true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
