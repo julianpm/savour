f4d032314d531303613a151c6c9ef77a748c8970
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-11-20 15:09:42 -0500

    removed unneeded code

--- footer.php
+++ footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'svr' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'svr' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'svr' ), 'svr', '<a href="http://underscores.me/" rel="designer">Julian Meanchoff</a>' ); ?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
