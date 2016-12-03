a7e25ca3ab09238d443cf27436524611db4f4f27
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-03 17:58:37 -0500

    made site branding a link

--- header.php
+++ header.php
@@ -69,7 +69,7 @@
 		<div class="row">
 			<div class="columns small-12 bottom-nav">
 				<div class="site-branding">
-					<h2><?php esc_html_e( 'savour.', 'svr' ); ?></h2>
+					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'savour.', 'svr' ); ?></a>
 				</div><!-- .site-branding -->
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
