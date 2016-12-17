e96ad2e8bcb0af5e15a23d93654f6de2d8eabb5c
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-16 23:26:34 -0500

    created about page template, began build out

---
+++ templates/about.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * TEMPLATE NAME: About
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package savour
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				// PAGE HEADER
+				svr_page_header();
+
+				// VALUES
+				svr_values();
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
