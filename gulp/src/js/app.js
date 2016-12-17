9e8ec15fba82f0f25f111c3666762256ebc63aea
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-16 22:50:51 -0500

    added search click function

--- gulp/src/js/app.js
+++ gulp/src/js/app.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function($){
 
+	$('.search a').click(function(event){
+		event.preventDefault();
+		$('#searchform').slideToggle();
+		$(this).children( 'i' ).toggleClass('fa-search fa-times');
+	});
+
 });
