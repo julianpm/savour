fdd90d2b5762317d1677bd4c1a4770afbdaab718
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-17 02:52:47 -0500

    - added page smooth scroll
    - added flyout nav click functionality

--- gulp/src/js/app.js
+++ gulp/src/js/app.js
@@ -1,9 +1,25 @@
 jQuery(document).ready(function($){
 
+	// SEARCH CLICK FUNCTION
 	$('.search a').click(function(event){
 		event.preventDefault();
 		$('#searchform').slideToggle();
 		$(this).children( 'i' ).toggleClass('fa-search fa-times');
 	});
+
+	// FLYOUT CLICK FUNCTION
+	$('.toggle-nav').click(function(){
+		$('.flyout-nav').animate({
+            right:0
+        }, 500, function(){
+        });
+	});
+
+	// PAGE NAVIGATION SCROLL
+  	$('.our-menus a').click(function(){
+    	var $link = $(this).attr('href');
+      	$("html, body").animate({ scrollTop: $($link).offset().top }, 600);
+      	return false;
+  	});
 
 });
