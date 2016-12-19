d4683253dfca4cba2c0d2b8279df1d400230cefc
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-19 10:24:12 -1000

    removed comment on search.php

--- search.php
+++ search.php
@@ -23,11 +23,6 @@ get_header(); ?>
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', 'search' );
 
 			endwhile;
@@ -44,5 +39,5 @@ get_header(); ?>
 	</section><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
