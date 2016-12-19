7159b76d88e5b29897f3b14a6c30c5c27086f6c6
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-18 22:48:40 -1000

    fixed markup for archive.php

--- archive.php
+++ archive.php
@@ -15,37 +15,37 @@ get_header(); ?>
 		<?php
 		if ( have_posts() ) : ?>
 
-			<header class="page-header">
+			<header class="page-header-simple section-padding">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
+				<div class="border"></div>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			<div class="row section-padding">
+				
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post(); ?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+					<div class="columns small-12 large-4">
 
-			endwhile;
+						<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-			the_posts_navigation();
+					</div>
 
-		else :
+				<?php
+				endwhile; ?>
+				
+			</div>
 
-			get_template_part( 'template-parts/content', 'none' );
-
+		<?php
 		endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
