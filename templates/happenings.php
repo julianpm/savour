c0de2f83d64daed37241d520371dfbc36b43b2b1
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-18 23:04:26 -1000

    building wp_query for happenings page template

--- templates/happenings.php
+++ templates/happenings.php
@@ -18,6 +18,40 @@ get_header(); ?>
 				// PAGE HEADER
 				svr_page_header();
 
+				// the query
+				$args = array(
+					'posts_per_page'	=> 3,
+					'order'				=> 'DESC',
+				);
+
+				$recent_posts = new WP_Query( $args ); ?>
+
+				<?php if ( $recent_posts->have_posts() ) : ?>
+
+					<!-- pagination here -->
+					<section class="section-padding">
+						<div class="row">
+
+							<!-- the loop -->
+							<?php while ( $recent_posts->have_posts() ) : $recent_posts->the_post(); ?>
+
+								<div class="columns small-12 large-4">
+									
+									<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+
+								</div>
+
+							<?php endwhile; ?>
+							<!-- end of the loop -->
+
+						<!-- pagination here -->
+						</div>
+					</section>
+
+					<?php wp_reset_postdata(); ?>
+
+				<?php endif;
+
 				// SUBSCRIBE CTA
 				svr_subscribe_cta();
 
