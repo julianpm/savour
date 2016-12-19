5b08c6e51d5400f378ec595a9ea4fafb144767e0
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-18 14:34:50 -1000

    removed the_content and added the_excerpt

--- template-parts/content.php
+++ template-parts/content.php
@@ -27,17 +27,6 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'svr' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'svr' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
