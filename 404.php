1f8a86fce09d797b5b1076f7c5b2822e546e85dc
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-12-23 22:30:26 -1000

    fixed markup for 404

--- 404.php
+++ 404.php
@@ -13,47 +13,16 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'svr' ); ?></h1>
+				<header class="page-header-simple section-padding">
+					<h1 class="page-title"><?php esc_html_e( '404', 'svr' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'svr' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( svr_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'svr' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'svr' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+				<div class="page-content section-padding">
+					<div class="row">
+						<div class="columns small-12">
+							<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'svr' ); ?></p>
+						</div>
+					</div>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
